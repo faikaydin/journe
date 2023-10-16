a7921dcad7835ca5b1d68c0881265db3e1593ddd
lezral <faikcanberkaydin@gmail.com>
2023-10-16 18:12:52 +1100

    sql file helper and config settings

--- app_config.py
+++ app_config.py
@@ -5,4 +5,5 @@ from src import utils
 CONFIG_FILE = os.path.join(os.path.dirname(__file__), 'config.yaml')
 config = utils.read_config(CONFIG_FILE)
 # path variables
-DATABASE_PATH = config['database_path']
+DATABASE_PATH = config['database_path']  # where the db will be
+JOURNE_CORE_CREATE_PATH = config['data_structure_sql_path']  # sql file to create/nuke db
