43a9b2ad25372d8453f57c4f109514e9993787af
lezral <faikcanberkaydin@gmail.com>
2023-10-16 18:33:59 +1100

    restructure the app_config and add package _init_ files

--- common/app_config.py
+++ common/app_config.py
@@ -2,7 +2,7 @@ import os
 from src import utils
 
 # handling config file
-CONFIG_FILE = os.path.join(os.path.dirname(__file__), 'config.yaml')
+CONFIG_FILE = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config.yaml')
 config = utils.read_config(CONFIG_FILE)
 # path variables
 DATABASE_PATH = config['database_path']  # where the db will be
