06d7b4050f3f7925a21956dd4b2d972196a88653
faikaydin <faikcanberkaydin@gmail.com>
2023-10-18 21:04:38 +1100

    adding default no_pot pot to core_db creation

--- src/data/journe_data_structure.sql
+++ src/data/journe_data_structure.sql
@@ -8,10 +8,13 @@ create table task (
   task_duration integer,
   task_pot_id text,
   foreign key (task_pot_id) references pot(pot_id)
-  );
+);
 -- pots
 create table pot (
   pot_id text primary key,
   pot_name text,
   pot_contents text[]
-  );
+);
+
+-- Insert the default pot entry
+INSERT INTO pot (pot_id, pot_name, pot_contents) VALUES ('0', 'no_pot', NULL);
